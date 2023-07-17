cdbb4bb93438830fba71ec34a9928acde34d2841
Alexis Drain <alexisdrain97@gmail.com>
2023-07-17 16:49:34 -0700

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        if (h != 0) {
+            GetComponent<Rigidbody>().AddForce(h * transform.right * 10f);
+        }
+        if (v != 0) {
+            GetComponent<Rigidbody>().AddForce(v * transform.forward * 10f);
+        }
+    }
+}
