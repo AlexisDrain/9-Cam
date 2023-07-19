cd121665d0c6edfaeee89d973beb7e59025aa4cd
Alexis Drain <alexisdrain97@gmail.com>
2023-07-19 14:04:22 -0700

    Spikes

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float roationSpeed = 1.8f;
+    public float forwardSpeed = 7f;
+    public float maxVelocity = 7f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +20,15 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         if (h != 0) {
-            transform.eulerAngles += new Vector3(0, h * 1.8f, 0);
+            transform.eulerAngles += new Vector3(0, h * roationSpeed, 0);
         }
         if (v != 0) {
-            GetComponent<Rigidbody>().AddForce(v * transform.forward * 7f);
+            GetComponent<Rigidbody>().AddForce(v * transform.forward * forwardSpeed, ForceMode.Impulse);
+            GetComponent<Rigidbody>().velocity = Vector3.ClampMagnitude(GetComponent<Rigidbody>().velocity, maxVelocity);
+            //GetComponent<Rigidbody>().velocity = v * transform.forward * forwardSpeed;
+            //GetComponent<Rigidbody>().AddForce(v * transform.forward * forwardSpeed);
+        } else {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
     }
 }
