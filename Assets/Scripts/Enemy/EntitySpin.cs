ebbc282acde700a7428368e27721f337d8a80746
Alexis Drain <alexisdrain97@gmail.com>
2023-08-23 21:57:49 -0700

    chainsaw sfx

--- Assets/Scripts/Enemy/EntitySpin.cs
+++ Assets/Scripts/Enemy/EntitySpin.cs
@@ -35,7 +35,7 @@ public class EntitySpin : MonoBehaviour
         } else {
             speed = Random.Range(spinSpeedRange.x, spinSpeedRange.y);
         }
-        print(speed);
+
         myRigidbody.AddTorque(speed * rotationAngle);
 
     }
